6dcd9e6cff2fb049e9050974e673fa9abbc20c7d
Louis Moses <louismoses.rait@gmail.com>
2023-11-08 09:59:58 +0800

    simple music player

--- src/components/AudioFile.jsx
+++ src/components/AudioFile.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const AudioFile = () => {
-  return <audio>123 </audio>;
+  return (
+    <>
+      <audio controls>
+        <source src="horse.ogg" type="audio/ogg" />
+        <source src="horse.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </>
+  );
 };
 
 export default AudioFile;
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,12 +1,20 @@
-import Title from "./Title";
+import Content from "./Content";
 import AudioFile from "./AudioFile";
 
 const MusicPlayer = () => {
   return (
-    <container className="flex min-h-[100vh]">
-      <Title title="THis is Music" artist="Lorem ipsum" artCover="" />
+    <div
+      className="flex flex-col gap-2 min-h-screen min-w-full border-8 place-content-center items-center mainContainer
+    "
+    >
+      <Content
+        title="Song Title"
+        artist="Artist Name"
+        artCover="https://i1.sndcdn.com/artworks-s8HcrEyRQPjzKLah-PT8lOg-t500x500.jpg"
+      />
+
       <AudioFile />
-    </container>
+    </div>
   );
 };
 
